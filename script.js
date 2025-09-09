5eb11f896a84e19b2393114b8a6a1350f8926d25
Austin <ausbratton@gmail.com>
2025-09-09 13:54:11 -0500

    Adjust display font size based on equation length

--- script.js
+++ script.js
@@ -60,6 +60,9 @@ function updateDisplayOnBtnClick(clickedBtn) {
         historyDisplay.textContent = mainDisplay.textContent.concat("=", answer);
         mainDisplay.textContent = answer.concat(clickedBtn.textContent);
     }
+
+    mainDisplay.style.fontSize = "64px";
+    formatDisplay();
 }
 
 function evaluateCurrentEquationSituation(mainDisplay) {
@@ -269,6 +272,20 @@ function calculateRemainder(mainDisplay) {
     return (firstNumber % secondNumber).toString();
 }
 
+function formatDisplay() {
+    const displayDiv = document.getElementById("display");
+    const mainDisplayDiv = document.getElementById("main-display");
+    const displayWidth = displayDiv.offsetWidth;
+    let mainDisplayWidth = mainDisplayDiv.offsetWidth;
+    let mainDisplayFontSize = parseFloat(window.getComputedStyle(mainDisplayDiv).fontSize);
+
+    while (mainDisplayWidth > displayWidth) {
+        mainDisplayDiv.style.fontSize = (mainDisplayFontSize - 1) + "px";
+        mainDisplayWidth = mainDisplayDiv.offsetWidth;
+        mainDisplayFontSize--;
+    }
+}
+
 document.addEventListener("DOMContentLoaded", function () {
     document.querySelectorAll(".keypad-btn").forEach((btn) => {
         btn.addEventListener("click", function (event) {
@@ -277,55 +294,7 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 
-// Button Categories:
-//      - Digit (0-9)
-//      - Basic Operators (Add, Subtract, Multiply, Divide)
-//      - Modulo Operator* (%)
-//      - Evaluator (=)
-//      - All Clear Function (AC)
-//      ( - Change Sign Function (+/-) )
-//      ( - Decimal Function (.) )
-//      ( - Backspace Function )
-//      ( - Info Button (i) )
-
-//      ( Category names in parentheses are future addtions. Not accounted for in equation situations yet. )
-
-// Equation Situations
-//      - Zero (0)
-//          - Digit click: Replace 0 with digit value (9)
-//          - Basic Operator OR Modulo Operator click: Append operator to equation string (0+)
-//          - Evaluator OR All Clear click: Do nothing (0)
-//
-//      - One Number (15)
-//          - Digit OR Basic Operator OR Modulo Operator click: Append digit value or operator to equation string (159 or 15+)
-//          - Evaluator click: Do nothing (15)
-//          - All Clear click: Reset equation string to 0 (0)
-
-//      - One Number with Trailing Basic Operator (15+)
-//          - Digit click: Append digit value to equation string (15+5)
-//          - Basic Operator OR Modulo Operator click: Replace equation string operator with clicked operator (15-)
-//          - Evaluator click: Do nothing (15+)
-//          - All Clear click: Reset equation string to 0 (0)
-
-//      - One Number with Trailing Modulo Operator (15%)
-//          - Digit click: Append digit value to equation string (15%9)
-//          - Basic Operator OR Modulo Operator click: Append operator to equation string (15%+) (On Modulo click, first number becomes percentage: (15%)%)
-//          - Evaluator click: Evaluate equation string(*1) (Calculate 15/100), replace equation string with evaluation answer (0.15), push evaluated equation string to display history (15%=0.15)
-//          - All Clear click: Reset equation string to 0 (0)
-
-//      - Numbers on Either Side of Basic Operator (15+5)
-//          - Digit click: Append digit value to equation string (15+51)
-//          - Basic Operator OR Modulo Operator click: Evaluate equation string (Calculate 15+5), replace equation string with evaluation answer and clicked operator appended (20-), push evaluated equation string to display history (15+5=20)
-//          - Evaluator click: Evaluate equation string (Calculate 15+5), replace equation string with evaluation answer (20), push evaluated equation string to display history (15+5=20)
-//          - All Clear click: Reset equation string to 0 (0)
-
-//      - Numbers on Either Side of Modulo Operator (15%6)
-//          - Digit click: Append digit value to equation string (15%69)
-//          - Basic Operator OR Modulo Operator click: Evaluate equation string(*4-a) (Calculate 15%6), replace equation string with with evaluation answer and clicked operator appended (3+), push evaluated equation string to display history
-//          - Evaluator click: Evaluate equation string (15%6), replace equation string with evaluation answer (3), push evaluated equation string to display history (15%6=3)
-//          - All Clear click: Reset equation string to 0 (0)
-
-// * Note: Modulo (%) does different operations depending on the context of the equation string. See examples below.
-//          *1 One number with trailing modulo: Evaluates to the decimal value of the number as a percent (15%=0.15)
-//          *2 Numbers on either side of modulo: Evaluates to the modulo of the equation (15%4=3)
-//          *3 Full equation string with trailing modulo on first number: Evaluate equation using decimal value of first number's percentage (60%+15=15.6)
+// Get display div width
+// Get main-display div width
+// If main-display width > display width:
+        // Decrease main-display font size until main-display width < display width
